1d7a6e62bd5a536e0e72878bdeaf750085ebe3a0
Cross Lee <timing.uniq@gmail.com>
2024-01-05 17:30:41 +0900

    feat: compose style theme

--- compose-tutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ compose-tutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -27,6 +29,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
@@ -100,7 +103,12 @@ fun Greeting(name: String) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold,
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -144,5 +152,19 @@ fun OnboardingPreview() {
 fun GreetingPreview() {
     ComposeTutorialTheme {
         MyApp()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview(){
+    ComposeTutorialTheme{
+        Greetings()
     }
 }
